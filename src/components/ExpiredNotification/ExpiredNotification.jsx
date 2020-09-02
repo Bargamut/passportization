dc59d258e9b988b12d3d93d7948737b80e861517
Paul Petrov <paul@bargamut.ru>
2020-09-03 01:26:55 +0300

    Create ExpiredNotification component

---
+++ src/components/ExpiredNotification/ExpiredNotification.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+const ExpiredNotification = () => (
+  <>
+    <p>
+      Срок опроса <b>завершён 1 сентября</b>.<br />
+      Спасибо всем, кто принял участие!
+    </p>
+
+    <h3>Не пропустите:</h3>
+
+    <p>Обновлённый вариант стартует 1 января 2021 года!</p>
+  </>
+);
+
+export default ExpiredNotification;
