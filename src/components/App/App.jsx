2a9260c539826103e0fb82e8497eec84538140f2
Paul Petrov <paul@bargamut.ru>
2020-09-03 03:00:44 +0300

    Update App component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,144 +13,166 @@ import HumansData from '../../providers/humans-data';
 import PublicsData from '../../providers/publics-data';
 import ExpiredNotification from '../ExpiredNotification/ExpiredNotification';
 
-const App = () => (
-  <>
-    {process.env.NODE_ENV === `production` &&
-      <YMInitializer
-        accounts={[ 47849594 ]}
-        options={{ clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }}
-        version="2"
-      />
-    }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-    <div className="wrapper">
-      <Header />
+    this.curentTimestamp = Date.now();
+    this.sessionStartTimestamp = new Date(`01-01-2021`).getTime();
+    this.sessionFinishTimestamp = new Date(`01-03-2021`).getTime();
+    this.isActiveSession =
+      this.curentTimestamp >= this.sessionStartTimestamp &&
+      this.curentTimestamp <= this.sessionFinishTimestamp;
+  }
 
-      <main className="site-body">
-        <BodyPart header="Внимание" className="body-part-warning">
-          <ExpiredNotification />
-        </BodyPart>
+  render() {
+    return (
+      <>
+        {process.env.NODE_ENV === `production` &&
+          <YMInitializer
+            accounts={[ 47849594 ]}
+            options={{ clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }}
+            version="2"
+          />
+        }
 
-        <BodyPart header="Что это?">
-          <p>Проект частного исследования:</p>
+        <div className="wrapper">
+          <Header />
 
-          <p className="pulse-question">
-            Актуальность паспортизации среди реконструкторов
-          </p>
+          <main className="site-body">
+            <BodyPart header="Что это?">
+              <p>Проект частного исследования:</p>
 
-          <p>
-            Это пробный выпуск, первый шаг для развития проекта ежегодного опроса среди сообщества реконструкторов - "Пульс паспортизации".
-          </p>
-        </BodyPart>
+              <p className="pulse-question">
+                Актуальность паспортизации среди реконструкторов
+              </p>
 
-        <hr className="break-line" />
+              <p>
+                Это пробный выпуск, первый шаг для развития проекта ежегодного опроса среди сообщества реконструкторов - "Пульс паспортизации".
+              </p>
+            </BodyPart>
 
-        <BodyCall header="Ваш опыт ценен!" image="/assets/decor/icons/vesy.png">
-          <div className="pulse-balance">
-            <div className="pulse-balance__item balance-item__left">Пишете паспорта?</div>
-            <div className="pulse-balance__item balance-item__right">Отказались от них?</div>
-          </div>
+            <hr className="break-line" />
 
-          <p>У вас наверняка есть причины - поделитесь этим!</p>
+            {!this.isActiveSession
+              ? (
+                <BodyPart header="Внимание" className="body-part-warning">
+                  <ExpiredNotification />
+                </BodyPart>
+                )
+              : (
+                <BodyCall header="Ваш опыт ценен!" image="/assets/decor/icons/vesy.png">
+                  <div className="pulse-balance">
+                    <div className="pulse-balance__item balance-item__left">Пишете паспорта?</div>
+                    <div className="pulse-balance__item balance-item__right">Отказались от них?</div>
+                  </div>
 
-          <CallToAction caption="Пройти опрос" disabled />
-        </BodyCall>
+                  <p>У вас наверняка есть причины - поделитесь этим!</p>
 
-        <hr className="break-line" />
+                  <CallToAction caption="Пройти опрос" disabled={!this.isActiveSession} />
+                </BodyCall>
+              )
+            }
 
-        <BodyPart header="Зачем?">
-          <ul className="items-list">
-            <li className="items-list__item">
-              Cобрать обезличенные данные о том, каким образом люди работают с паспортами комплектов.
-            </li>
+            <hr className="break-line" />
 
-            <li className="items-list__item">
-              Сформировать общую картину паспортизации в современной реконструкции в России.
-            </li>
+            <BodyPart header="Зачем?">
+              <ul className="items-list">
+                <li className="items-list__item">
+                  Cобрать обезличенные данные о том, каким образом люди работают с паспортами комплектов.
+                </li>
 
-            <li className="items-list__item">
-              Узнать о проблемах в паспортизации и, быть может, найти их решения.
-            </li>
+                <li className="items-list__item">
+                  Сформировать общую картину паспортизации в современной реконструкции в России.
+                </li>
 
-            <li className="items-list__item">
-              Сделать на основе этого проекта ежегодный "пульс", более грамотный с точки зрения социологии. Сейчас проект в экспериментальном режиме.
-            </li>
-          </ul>
-        </BodyPart>
+                <li className="items-list__item">
+                  Узнать о проблемах в паспортизации и, быть может, найти их решения.
+                </li>
 
-        <BodyPart header="А это безопасно?">
-          <p>Здесь не просят ваших имён, адрес, почту или номер телефона. Нет регистрирации и SMS.</p>
+                <li className="items-list__item">
+                  Сделать на основе этого проекта ежегодный "пульс", более грамотный с точки зрения социологии. Сейчас проект в экспериментальном режиме.
+                </li>
+              </ul>
+            </BodyPart>
 
-          <p>Есть только 15 вопросов.</p>
+            <BodyPart header="А это безопасно?">
+              <p>Здесь не просят ваших имён, адрес, почту или номер телефона. Нет регистрирации и SMS.</p>
 
-          <p>Все ответы анонимны.</p>
+              <p>Есть только 15 вопросов.</p>
 
-          <div className="call-to-action">
-            <CallToAction caption="Пройти опрос" disabled />
-          </div>
+              <p>Все ответы анонимны.</p>
 
-          <p>
-            Когда опрос закончится, все ответы будут сведены и опубликованы на отдельной странице в виде инфографики "о состоянии паспортизации в 2020 году" на сайте проекта.
-          </p>
-        </BodyPart>
+              {this.isActiveSession &&
+                <div className="call-to-action">
+                  <CallToAction caption="Пройти опрос" disabled={!this.isActiveSession} />
+                </div>
+              }
 
-        <hr className="break-line" />
+              <p>
+                Когда опрос закончится, все ответы будут сведены и опубликованы на отдельной странице в виде инфографики "о состоянии паспортизации в 2020 году" на сайте проекта.
+              </p>
+            </BodyPart>
 
-        <BodyCall header="Знаете что можно улучшить?" image="/assets/decor/icons/lamp.svg" >
-          <p>
-            Проект молодой и ему важна ваша поддержка.
-          </p>
+            <hr className="break-line" />
 
-          <p>
-            Я открыт для идей, предложений и замечаний: пожалуйста, свяжитесь со мной!
-          </p>
+            <BodyCall header="Знаете что можно улучшить?" image="/assets/decor/icons/lamp.svg" >
+              <p>
+                Проект молодой и ему важна ваша поддержка.
+              </p>
 
-          <div className="call-to-action">
-            <ButtonLink className="button--dark" href="https://t.me/bargamut" target="_blank">
-              Telegram
-            </ButtonLink>
+              <p>
+                Я открыт для идей, предложений и замечаний: пожалуйста, свяжитесь со мной!
+              </p>
 
-            <ButtonLink className="button--dark" href="https://vk.bargamut.ru" target="_blank">
-              ВКонтакте
-            </ButtonLink>
+              <div className="call-to-action">
+                <ButtonLink className="button--dark" href="https://t.me/bargamut" target="_blank">
+                  Telegram
+                </ButtonLink>
 
-            <ButtonLink className="button--dark" href="https://fb.bargamut.ru" target="_blank">
-              Facebook
-            </ButtonLink>
-          </div>
-        </BodyCall>
+                <ButtonLink className="button--dark" href="https://vk.bargamut.ru" target="_blank">
+                  ВКонтакте
+                </ButtonLink>
 
-        <hr className="break-line" />
+                <ButtonLink className="button--dark" href="https://fb.bargamut.ru" target="_blank">
+                  Facebook
+                </ButtonLink>
+              </div>
+            </BodyCall>
 
-        <BodyCall header="Самый простой способ поддержать">
-          <p>
-            Поделитесь этим сайтом с коллегами-реконструкторами: чем больше реконструкторов примет участие,
-            тем достовернее будет результат.
-          </p>
+            <hr className="break-line" />
 
-          <SocialsShare />
-        </BodyCall>
+            <BodyCall header="Самый простой способ поддержать">
+              <p>
+                Поделитесь этим сайтом с коллегами-реконструкторами: чем больше реконструкторов примет участие,
+                тем достовернее будет результат.
+              </p>
 
-        <hr className="break-line" />
+              <SocialsShare />
+            </BodyCall>
 
-        <BodyPart header="Помощники">
-          <h3 className="body-part__subhead">Помощь в подготовке</h3>
+            <hr className="break-line" />
 
-          <CardsList provider={new HumansData()} />
+            <BodyPart header="Помощники">
+              <h3 className="body-part__subhead">Помощь в подготовке</h3>
+
+              <CardsList provider={new HumansData()} />
+
+              <hr className="break-line" />
+
+              <h3 className="body-part__subhead">Информационная поддержка</h3>
+
+              <CardsList provider={new PublicsData()} mode="public" />
+            </BodyPart>
+          </main>
 
           <hr className="break-line" />
 
-          <h3 className="body-part__subhead">Информационная поддержка</h3>
-
-          <CardsList provider={new PublicsData()} mode="public" />
-        </BodyPart>
-      </main>
-
-      <hr className="break-line" />
-
-      <Footer />
-    </div>
-  </>
-);
+          <Footer />
+        </div>
+      </>
+    );
+  }
+}
 
 export default App;
